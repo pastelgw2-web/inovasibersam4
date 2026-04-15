4e6580119409fc25bd6e84dfb461da09f13e7ec0
pastelgw2-web <pastelgw2@gmail.com>
2026-04-15 09:07:39 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,6 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3000,
-    host: '0.0.0.0',
-  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, '.'),
